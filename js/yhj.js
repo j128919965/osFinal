d44a19c673b26fd291802cd653cd2a2471a1d319
SBYQWDZNB <910506129@qq.com>
2020-06-29 17:30:04 +0800

    629

--- js/yhj.js
+++ js/yhj.js
@@ -8,6 +8,7 @@ let request=[];
 let safe_sequence=[]; //安全序列
 let safelist=[]; //安全序列列表
 let flag = false;
+let cnt=1;
 
 const get = (e)=>{return document.querySelector(e)};
 
@@ -40,14 +41,17 @@ const submit = ()=>{
     
     let p = get("#pro-id").value;
     let request = get("#request").value;
+    console.log(p,request)
 
     let proList = new Array(process_num);
 
     for(let i = 0; i< process_num ;i++){
-        proList[i] = new process(i,allocation[i],max[i],p===i?request:0);
+        console.log(p,i,p===i)
+        proList[i] = new process(i,allocation[i],max[i],p===i?request:0);    
+        console.log(proList[i])
     }
-
-    findsafelist(proList,aviliable);
+    cnt=1;
+    slv(proList,aviliable);
 
     // for(let i = 0;i<safe_sequence.length;i++){
     //     console.log(safe_sequence[i])
@@ -77,8 +81,8 @@ class process{
      * @param {*} aviliable 
      */
     islegale=()=>{
-        for(let i in need){
-            if(need[i]<request[i])
+        for(let i in this.need){
+            if(this.need[i]<this.request[i])
                 return false
         }
         return true
@@ -87,8 +91,8 @@ class process{
      * 判断avilable能否满足该进程的request
      */
     isrequested=(aviliable)=>{
-        for(let i in request){
-            if(request[i]>aviliable[i])
+        for(let i in this.request){
+            if(this.request[i]>aviliable[i])
                 return false
         }
         return true
@@ -97,55 +101,73 @@ class process{
      * 判断aviliable能否满足进程的need
      */
     issafe=(aviliable)=>{
-        for(let i in need){
-            if(need[i]>aviliable[i])
+        for(let i in this.need){
+            if(this.need[i]>aviliable[i])
                 return false
         }
         return true
     }
 }
 
+function slv(prolist,aviliable){
+    for(let i in prolist){
+        if(!prolist[i].islegale()){
+            alert("进程："+i+"存在错误，request大于need")
+            break
+        }
+        if(!prolist[i].isrequested(aviliable)){
+            console.log("进程："+i+"无法满足request")
+            continue
+        }
+        for(let j in aviliable){ //把进程的request加上
+            aviliable[j]-=prolist[i].request[j]
+            prolist[i].need[j]-=prolist[i].request[j]
+            prolist[i].allocation[j]+=prolist[i].request[j]
+        }
+    }
+    //console.log(prolist)
+    findsafelist(prolist,aviliable)
+}
 /**
  * 寻找安全序列
  * @param {*} prolist //进程列表 
  * @param {*} aviliable //可用资源数列表
  */
 function findsafelist(prolist,aviliable){
+    //console.log(aviliable)
     let length=prolist.length
-    for(i in prolist){
-        if(prolist[i].finish)
+    
+    for(let i in prolist){
+        if(prolist[i].finish){
+            //console.log("进程："+i+"已完成")
             continue
-        if(!prolist[i].islegale()){
-            alert("存在错误，request大于need")
-            break
-        }
-        if(!prolist[i].isrequested(aviliable)){
-            continue
-        }
-        for(j in aviliable){ //把进程的request加上
-            aviliable[j]-=prolist[i].request[j]
-            prolist[i].need[j]-=prolist[i].request[j]
-            prolist[i].allocation[j]+=prolist[i].request[j]
         }
         //进程无法执行，跳过
         if(!prolist[i].issafe(aviliable)){
+            //console.log("进程："+i+"无法满足need")
             continue;
         }
         //进程可以执行，将进程加入安全序列，并释放进程占用的资源
+        //console.log("进程："+i+"进入安全序列")
         prolist[i].finish=true;
         safe_sequence.push(prolist[i].id); 
-        for(j in aviliable){
+        for(let j in aviliable){
             aviliable[j]+=prolist[i].allocation[j]
         }
         findsafelist(prolist,aviliable);
+        //console.log("进程："+i+"退出安全序列")
         safe_sequence.splice(safe_sequence.length-1,1)
+        //console.log(safe_sequence)
         prolist[i].finish=false;
-        for(j in aviliable){
+        for(let j in aviliable){
             aviliable[j]-=prolist[i].allocation[j]
         }
     }
     if(safe_sequence.length==length){
-        safelist.push(safe_sequence)
+        console.log("安全序列"+cnt+": "+safe_sequence)
+        cnt++;
+        //safelist.push(safe_sequence)
+        //console.log(safelist)
     }
     return
 }
